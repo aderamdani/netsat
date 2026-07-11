db77baef3ca4a24deee2d2f2d3f4581234d293a1
Ade Ramdani <mr.aderamdani@gmail.com>
2026-07-11 09:53:00 +0900

    refactor(ui): hapus fitur interaktif yang tidak perlu di landing page

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -11,9 +11,6 @@ import './custom.css'
 import HomeHero from './components/HomeHero.vue'
 import ModuleGrid from './components/ModuleGrid.vue'
 import TelemetryStrip from './components/TelemetryStrip.vue'
-import TerminalSimulator from './components/TerminalSimulator.vue'
-import SubnetWidget from './components/SubnetWidget.vue'
-import PacketTracerMini from './components/PacketTracerMini.vue'
 
 export default {
   extends: DefaultTheme,
@@ -21,9 +18,6 @@ export default {
     app.component('HomeHero', HomeHero)
     app.component('ModuleGrid', ModuleGrid)
     app.component('TelemetryStrip', TelemetryStrip)
-    app.component('TerminalSimulator', TerminalSimulator)
-    app.component('SubnetWidget', SubnetWidget)
-    app.component('PacketTracerMini', PacketTracerMini)
 
     // View Transitions API
     if (typeof window !== 'undefined' && 'startViewTransition' in document) {
